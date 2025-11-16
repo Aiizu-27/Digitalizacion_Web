35ce23fb9f933d7e2396b9bb98084eacfa0919e1
aiizu-27 <luciiacampos.27@gmail.com>
2025-11-16 15:44:33 +0100

    php

---
+++ html/novedades/novedades.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novedades - DAILY DOSE</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/variables.css">
+<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+
+<!-- Header / Cabecera de la página -->
+<?php include "includes/header.php"; ?>
+
+<!-- Enlace al JS externo -->
+<script src="js/script.js"></script>
+</body>
+</html>
