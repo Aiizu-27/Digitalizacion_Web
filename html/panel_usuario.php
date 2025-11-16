c64a35bf7fc73b211e300a8e707d3b933c0b985a
aiizu-27 <luciiacampos.27@gmail.com>
2025-11-16 18:51:03 +0100

    arreglo panel usuario

--- html/panel_usuario.php
+++ html/panel_usuario.php
@@ -36,6 +36,7 @@ $conn->close();
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Panel de Usuario - DAILY DOSE</title>
+<link rel="stylesheet" href="css/variables.css">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/header.css">
 <link rel="stylesheet" href="css/footer.css">
