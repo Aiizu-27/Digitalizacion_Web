b373c499733b76fe3b3ff5c763203e229a836ffd
aiizu-27 <luciiacampos.27@gmail.com>
2026-01-30 22:22:29 +0100

    ahora¿?

--- html/includes/config.php
+++ html/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 // ===== CONFIGURACIÓN DE BASE DE DATOS =====
-$host = "192.168.0.76";       // Cambia si usas otro host
+$host = "localhost";       // Cambia si usas otro host
 //$host = "dailydosedb.cfw8im4q8gip.eu-south-2.rds.amazonaws.com";
 $usuario = "admin_dd";         // Tu usuario MySQL
 $contrasena = "271304Lu";    // Tu contraseña
