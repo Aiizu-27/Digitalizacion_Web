d030ca4af063f63877f0784262034e1ab10bf060
Aiizu-27 <luciiiia.2713@gmail.com>
2026-02-03 00:46:33 +0100

    ERRORCITOS

--- html/actions/auth_login.php
+++ html/actions/auth_login.php
@@ -26,11 +26,12 @@ if ($resultado->num_rows > 0) {
     if (password_verify($pass, $usuario['CONTRASENA'])) {
 
         $_SESSION['ID_USUARIO'] = $usuario['ID_USUARIO'];
-        $_SESSION['NOMBRE'] = $usuario['nombre'];
+        $_SESSION['NOMBRE'] = $usuario['NOMBRE'];
         echo "login_ok";
     } else {
         echo "usuario_no_encontrado";
     }
+}
 
 $stmt->close();
 $conn->close();
