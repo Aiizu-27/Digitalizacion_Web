bd8042a789c45f9ced83ed6daef4a94c930b3a2e
Aiizu-27 <luciiiia.2713@gmail.com>
2026-02-10 18:00:23 +0100

    arreglo consulta select panel_usuarios

--- html/panel/panel.php
+++ html/panel/panel.php
@@ -10,11 +10,11 @@ if(!isset($_SESSION['ID_CLIENTE'])){
 }
 
 // 2. Conexión centralizada
-require_once "includes/config.php"; 
+require_once "../includes/config.php"; 
 
 // 3. Obtener datos del usuario
 $id_cliente = $_SESSION['ID_CLIENTE'];
-$stmt = $conn->prepare("SELECT NOMBRE, CORREO, TELEFONO, PUNTOS FROM CLIENTES WHERE ID_CLIENTE = ?");
+$stmt = $conn->prepare("SELECT NOMBRE, APELLIDOS, EMAIL, TELEFONO, PUNTOS FROM USUARIOS JOIN CLIENTES ON USUARIOS.ID_USUARIO = CLIENTES.ID_USUARIO WHERE USUARIOS.ID_USUARIO = ?");
 $stmt->bind_param("i", $id_cliente);
 $stmt->execute();
 $result = $stmt->get_result();
@@ -27,4 +27,4 @@ $conn->close();
 // 5. CARGAR LA VISTA (El HTML)
 // Las variables creadas arriba ($usuario_info) estarán disponibles en este archivo
 include "../views/panel_vista.php";
-?>
+?> 
